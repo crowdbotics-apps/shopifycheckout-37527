812293268624572a5f9a6277a3538b1e7feebd0b
saprabotics <116142524+saprabotics@users.noreply.github.com>
2022-11-10 16:37:58 +0500

    commented code for cb deployment.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
 
@@ -41,5 +41,10 @@ Route::get('/', function () {
     // return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
 
+*/
+
+Route::get('/', function () {
+    echo "<h1>Test</h1>";
+});
 
 Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
